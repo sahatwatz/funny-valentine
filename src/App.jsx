58e397a341c61471d08d9664980da4b73cb209e6
sahatwatz <yoke.21@hotmail.com>
2025-02-11 18:45:37 +0700

    config style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import "./App.css";
+import { useState } from "react";
+import NameInput from "./component/NameInput"; // นำเข้า NameInput
+import WelcomeScreen from "./component/WelcomeScreen"; // นำเข้า WelcomeScreen
 
-function App() {
+export default function App() {
+  const [name, setName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   return (
-    <>
-      <div className="h-screen flex items-center justify-center bg-pink-300 text-white">
-        <h1 className="text-3xl font-bold"> What is your name </h1>
-      </div>
-    </>
+    <div className="flex justify-center items-center h-screen ">
+      {!isSubmitted ? (
+        <NameInput
+          onConfirm={(name) => {
+            setName(name);
+            setIsSubmitted(true);
+          }}
+        />
+      ) : (
+        <WelcomeScreen name={name} />
+      )}
+    </div>
   );
 }
-
-export default App;
