2cd54e196712c2303b442f875e0452cdcf7a44e5
sahatwatz <yoke.21@hotmail.com>
2025-02-11 17:25:20 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-pink-300 text-white">
+        <h1 className="text-3xl font-bold"> What is your name </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
